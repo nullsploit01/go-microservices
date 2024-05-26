87db53a7b243b0eeee8bc3fc564283dc5625fbf1
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-27 02:06:13 +0530

    add a helper for writing http responses

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -2,15 +2,7 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/nullsploit01/go-microservices/broker/common/util"
 )
-
-type Response struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := Response{
@@ -18,5 +10,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Broker says what?",
 	}
 
-	util.WriteJSON(w, http.StatusOK, payload)
+	_ = app.writeJson(w, http.StatusOK, payload)
 }
---
+++ broker-service/cmd/api/helpers.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type Response struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) readJson(w http.ResponseWriter, r *http.Request, data any) error {
+	maxSize := 1048576 // 1MB
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxSize))
+
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(data); err != nil {
+		return err
+	}
+
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		return errors.New("body must have single JSON object")
+	}
+
+	return nil
+}
+
+func (app *Config) writeJson(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *Config) errorJson(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload Response
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJson(w, statusCode, payload)
+}
