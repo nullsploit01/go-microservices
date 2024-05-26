4ce7153303225c33d3db7ff176bcda19822d4158
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-27 02:52:16 +0530

    add ability to connect db

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/nullsploit01/go-microservices/authentication/data"
 )
 
 const webPort = "80"
+
+var dbConnectCounts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -17,7 +24,16 @@ type Config struct {
 }
 
 func main() {
-	app := Config{}
+	conn := connectDB()
+
+	if conn == nil {
+		log.Panic("Cant connect to DB")
+	}
+
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -26,5 +42,43 @@ func main() {
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		conn, err := openDB(dsn)
+		if err != nil {
+			log.Panicln("Connecting to DB failed")
+			dbConnectCounts += 1
+		} else {
+			log.Println("Connected to DB")
+			return conn
+		}
+
+		if dbConnectCounts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Connecting to Database....")
+		time.Sleep(2 * time.Second)
+		continue
 	}
 }
