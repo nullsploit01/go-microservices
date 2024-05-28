596c5c8203e38ddfd6a11bbf7c17a3a332d047eb
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-29 02:23:54 +0530

    add methods t update and drop collection

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -60,7 +61,7 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 	collection := client.Database("logs").Collection("logs")
 
 	options := options.Find()
-	options.SetSort(bson.D{{"created_at", -1}})
+	options.SetSort(bson.D{{Key: "created_at", Value: -1}})
 
 	cursor, err := collection.Find(ctx, bson.D{}, options)
 	if err != nil {
@@ -82,4 +83,68 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 	}
 
 	return logs, nil
+}
+
+func (l *LogEntry) GetOne(id string) (*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*12)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Println("Error getting log entry", err)
+		return nil, err
+	}
+
+	var entry LogEntry
+
+	err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&entry)
+	if err != nil {
+		log.Println("Error getting log entry", err)
+		return nil, err
+	}
+
+	return &entry, nil
+}
+
+func (l *LogEntry) DropCollection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*12)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	if err := collection.Drop(ctx); err != nil {
+		log.Println("Error dropping collection", err)
+		return err
+	}
+
+	return nil
+}
+
+func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*12)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	docID, err := primitive.ObjectIDFromHex(l.ID)
+	if err != nil {
+		log.Println("Error updating log entry", err)
+		return nil, err
+	}
+
+	result, err := collection.UpdateOne(ctx, bson.M{"_id": docID}, bson.D{
+		{Key: "$set", Value: bson.D{
+			{Key: "name", Value: l.Name},
+			{Key: "data", Value: l.Data},
+			{Key: "updated_at", Value: time.Now()},
+		}},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
