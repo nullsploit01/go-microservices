05bd513c8edde3b7863af229b58bce176a5b9c85
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-31 02:45:37 +0530

    add ability to connect rabbit mq

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"math"
+	"time"
 
+	"github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	rabbitmqConn, err := connect()
+	if err != nil {
+		panic(err)
+	}
+
+	defer rabbitmqConn.Close()
+}
+
+func connect() (*amqp091.Connection, error) {
+	var counts int64
+	var timeoff = 1 * time.Second
+	var conn *amqp091.Connection
+
+	for {
+		c, err := amqp091.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("Could not connect to rabbitmq, retrying")
+			counts += 1
+		} else {
+			conn = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		timeoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Printf("Waiting for %s seconds and retrying...", timeoff)
+		time.Sleep(timeoff)
+		continue
+	}
+
+	return conn, nil
 }
