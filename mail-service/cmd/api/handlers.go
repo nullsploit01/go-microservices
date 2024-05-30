5cbc9962e7d3e4d7262f782e5fb7553c0e24ba58
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-31 02:21:33 +0530

    log mail events

--- mail-service/cmd/api/handlers.go
+++ mail-service/cmd/api/handlers.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
 	type mailMessage struct {
@@ -34,5 +39,42 @@ func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
 		Message: "sent to " + requestPayload.To,
 	}
 
+	err = app.logRequest("mail", fmt.Sprintf("%s send email to %s", requestPayload.From, requestPayload.To))
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
 	app.writeJson(w, http.StatusOK, payload)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, err := json.MarshalIndent(entry, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	request, err := http.NewRequest("POST", "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	return nil
 }
