4f97a666f2d546358912bc579540c5440c93d7f3
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-31 01:16:35 +0530

    add handler to send mail

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
-type Config struct{}
+type Config struct {
+	Mailer Mail
+}
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	m, err := initMailer()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	app := Config{
+		Mailer: m,
+	}
+
 	log.Println("Started mail service on port", webPort)
 
 	server := &http.Server{
@@ -19,8 +31,28 @@ func main() {
 		Handler: app.routes(),
 	}
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func initMailer() (Mail, error) {
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		return Mail{}, err
+	}
+
+	mailer := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("MAIL_NAME"),
+		FromAddress: os.Getenv("MAIL_ADDRESS"),
+	}
+
+	return mailer, nil
 }
