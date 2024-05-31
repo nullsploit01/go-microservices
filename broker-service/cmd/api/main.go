6ecca23b8d0ad2d9f6b01cb3b14db2828d394e03
nullsploit01 <harshaldharmik099@gmail.com>
2024-06-01 02:35:53 +0530

    add event and consumer in broker service

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"time"
+
+	"github.com/rabbitmq/amqp091-go"
 )
 
 const webPort = "80"
 
-type Config struct{}
+type Config struct {
+	Rabbit amqp091.Connection
+}
 
 func main() {
-	app := Config{}
+	rabbitmqConn, err := connectRabbitMq()
+	if err != nil {
+		panic(err)
+	}
+
+	defer rabbitmqConn.Close()
+	app := Config{
+		Rabbit: *rabbitmqConn,
+	}
 
 	log.Printf("Started broker service on port %s\n", webPort)
 
@@ -23,4 +37,34 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Panic(err)
 	}
+}
+
+func connectRabbitMq() (*amqp091.Connection, error) {
+	var counts int64
+	var timeoff = 1 * time.Second
+	var conn *amqp091.Connection
+
+	for {
+		c, err := amqp091.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("Could not connect to rabbitmq, retrying")
+			counts += 1
+		} else {
+			fmt.Println("Connected to rabbit mq")
+			conn = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		timeoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Printf("Waiting for %s seconds and retrying...", timeoff)
+		time.Sleep(timeoff)
+		continue
+	}
+
+	return conn, nil
 }
