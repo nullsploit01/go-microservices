c71d6feefa5c3c3c92b10e3a9e867741b381002a
nullsploit01 <harshaldharmik099@gmail.com>
2024-06-01 02:45:40 +0530

    add event emitter service in broker service

---
+++ broker-service/event/emitter.go
@@ -0,0 +1,62 @@
+package event
+
+import (
+	"log"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type Emitter struct {
+	connection *amqp091.Connection
+}
+
+func (e *Emitter) setup() error {
+	channel, err := e.connection.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	return declareExchange(channel)
+}
+
+func (e *Emitter) Push(event string, severity string) error {
+	channel, err := e.connection.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer channel.Close()
+
+	log.Println("Pushing event to channel")
+
+	err = channel.Publish(
+		"logs_topic",
+		severity,
+		false,
+		false,
+		amqp091.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(event),
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewEventEmitter(conn *amqp091.Connection) (Emitter, error) {
+	emitter := Emitter{
+		connection: conn,
+	}
+
+	err := emitter.setup()
+	if err != nil {
+		return Emitter{}, err
+	}
+
+	return emitter, nil
+}
